ff3a0c82b033bebb9b8a95cad763271ed5e1d0af
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-19 16:04:54 -0400

    feat: process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def already_in(path_file: str, instance: Queue):
+    crr_value = instance.head_value
+    while (crr_value):
+        if (path_file == crr_value.value["nome_do_arquivo"]):
+            return True
+        crr_value = crr_value.next
+    return False
+
+
+def process(path_file: str, instance: Queue):
+    lines = txt_importer(path_file)
+
+    if (not already_in(path_file, instance)):
+        new_content = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(new_content)
+
+        return sys.stdout.write(str(new_content))
 
 
 def remove(instance):
