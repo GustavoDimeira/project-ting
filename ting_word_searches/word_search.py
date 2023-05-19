96e64b179983857dfa4f4d2bc56762000cbc3735
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-19 19:57:48 -0400

    feat: search by word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    response = []
+    crr_value = instance.head_value
+    while (crr_value):
+        ocurrences = []
+        print(crr_value.value["linhas_do_arquivo"])
+
+        for i in range(len(crr_value.value["linhas_do_arquivo"])):
+            line = crr_value.value["linhas_do_arquivo"][i]
+            if (word.lower() in line.lower()):
+                ocurrences.append({"linha": i + 1})
+
+        if (len(ocurrences)):
+            response.append({
+                "palavra": word,
+                "arquivo": crr_value.value["nome_do_arquivo"],
+                "ocorrencias": ocurrences
+            })
+        crr_value = crr_value.next
+    return response
 
 
 def search_by_word(word, instance):
