aad6b7bd65c001d8dd4951a2126b4e2c88abd233
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-19 20:01:08 -0400

    feat: search by word and get content

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -23,5 +23,26 @@ def exists_word(word: str, instance: Queue):
     return response
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue):
+    response = []
+    crr_value = instance.head_value
+    while (crr_value):
+        ocurrences = []
+        print(crr_value.value["linhas_do_arquivo"])
+
+        for i in range(len(crr_value.value["linhas_do_arquivo"])):
+            line = crr_value.value["linhas_do_arquivo"][i]
+            if (word.lower() in line.lower()):
+                ocurrences.append({
+                    "linha": i + 1,
+                    "conteudo": line
+                    })
+
+        if (len(ocurrences)):
+            response.append({
+                "palavra": word,
+                "arquivo": crr_value.value["nome_do_arquivo"],
+                "ocorrencias": ocurrences
+            })
+        crr_value = crr_value.next
+    return response
